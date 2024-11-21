9343243e3ac965bab2b6fe6f0e428c0a7c31822e
MayankGEHU <minku6195@gmail.com>
2024-11-21 12:38:57 +0530

    euler and runda kutta

--- Newton-BackWard.c
+++ Newton-BackWard.c
@@ -10,31 +10,28 @@ int main() {
     printf("Enter the Number of terms: ");
     scanf("%d", &n);
 
-    // Dynamic array allocation
     
     float arr[n][n + 1];
 
     printf("\nEnter the values of x: ");
     for (int i = 0; i < n; i++) {
-        scanf("%f", &arr[i][0]);  // x values
+        scanf("%f", &arr[i][0]);  
     }
 
     printf("\nEnter the values of y: ");
     for (int i = 0; i < n; i++) {
-        scanf("%f", &arr[i][1]);  // y values
+        scanf("%f", &arr[i][1]);  
     }
 
     printf("\nEnter the value of x for which you want y: ");
     scanf("%f", &x);
 
-    // Create the backward difference table
     for (int j = 2; j < n + 1; j++) {
         for (int i = n - 1; i >= j - 1; i--) {
             arr[i][j] = arr[i][j - 1] - arr[i - 1][j - 1];
         }
     }
 
-    // Print the backward difference table
     printf("\nBACKWARD DIFFERENCE TABLE\n\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n + 1 - i; j++) {
@@ -43,13 +40,10 @@ int main() {
         printf("\n");
     }
 
-    // Calculate u
     u = (x - arr[n - 1][0]) / (arr[1][0] - arr[0][0]);
     
-    // Initialize y with the last known y value
     y = arr[n - 1][1];
 
-    // Compute y using Newton's Backward Interpolation formula
     for (int i = 2; i <= n; i++) {
         y += (u * arr[n - 1][i]) / (i - 1);
         u *= (u + (i - 1));
